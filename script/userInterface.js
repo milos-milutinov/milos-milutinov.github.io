231eba67731292d4b2d4fc4f2bff038e33fc0666
Milos <milosmilutinov7@gmail.com>
2021-02-25 12:38:01 +0100

    commit

--- script/userInterface.js
+++ script/userInterface.js
@@ -2,7 +2,7 @@
 function main() {
     setTimeout(() => {
         let user = JSON.parse(localStorage.getItem("currentUSER"));
-        document.getElementById("loggedLink").textContent = user.user_name;
+        document.getElementById("yourCars").textContent = user.user_name;
 
     }, 500);
 }
